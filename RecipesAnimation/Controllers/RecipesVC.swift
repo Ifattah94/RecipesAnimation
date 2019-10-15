e6024e6a3019ff3effde62596ac1bde29a8ae6fe
Iram Fattah <iramfattah94@gmail.com>
2019-10-15 19:17:53 -0400

    configure flow layout for collection view

--- RecipesAnimation/Controllers/RecipesVC.swift
+++ RecipesAnimation/Controllers/RecipesVC.swift
@@ -7,7 +7,9 @@
 //
 
 import UIKit
-
+var itemSize = CGSize(width: 250, height: 350)
+ let cellSpacing = UIScreen.main.bounds.size.width * 0.09
+private let cellIdentifier = "RecipeCell"
 class RecipesVC: UIViewController {
 
     //MARK: UI Objects
@@ -19,12 +21,23 @@ class RecipesVC: UIViewController {
     }()
     
     lazy var recipesCollectionView: UICollectionView = {
-        let cv = UICollectionView(frame: CGRect.zero, collectionViewLayout: UICollectionViewFlowLayout())
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        let cv = UICollectionView(frame: CGRect.zero, collectionViewLayout: layout)
         cv.backgroundColor = .clear
-        cv.clipsToBounds = true
+        cv.register(RecipesCell.self, forCellWithReuseIdentifier: cellIdentifier)
+      
+        cv.dataSource = self
+        cv.delegate = self
         
         return cv
     }()
+    
+    
+    //MARK: Properties
+    
+    private let recipes = Recipe.buildRecipes()
+    
     
     
     override func viewDidLoad() {
@@ -53,4 +66,47 @@ class RecipesVC: UIViewController {
         
     }
 
+}
+
+extension RecipesVC: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return recipes.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath) as! RecipesCell
+        let recipe = recipes[indexPath.row]
+        cell.configureCell(with: recipe)
+        return cell
+        
+    }
+    
+
+    
+}
+
+extension RecipesVC: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let numCells: CGFloat = 0.8
+        let numSpaces: CGFloat = numCells + 2.9
+        
+        let screenWidth = UIScreen.main.bounds.width
+        let screenHeight = UIScreen.main.bounds.height
+        
+        return CGSize(width: (screenWidth - (cellSpacing * numSpaces)) / numCells, height: screenHeight * 0.7)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: cellSpacing, left: cellSpacing, bottom: 0, right: cellSpacing)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return cellSpacing
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return cellSpacing
+    }
+    
+    
 }
--- RecipesAnimation/Views/RecipesCell.swift
+++ RecipesAnimation/Views/RecipesCell.swift
@@ -12,7 +12,29 @@ class RecipesCell: UICollectionViewCell {
     
     lazy var recipeImageView: UIImageView = {
         let iv = UIImageView()
+        iv.clipsToBounds = true
+        iv.contentMode = .scaleAspectFill
+        iv.alpha = 0.7
         return iv
+    }()
+    
+    lazy var recipeNameLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.textColor = .black
+        label.font = UIFont(name: "Noteworthy-Bold", size: 20)
+        return label
+    }()
+    
+    lazy var descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.clipsToBounds = false
+         label.contentMode = .left
+        label.isHidden = true 
+        label.font = UIFont(name: "System", size: 17)
+        return label
     }()
     
     
@@ -21,10 +43,48 @@ class RecipesCell: UICollectionViewCell {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        configureRecipeImageViewConstraints()
+        configureRecipeNameLabel()
+        configureDescriptionLabelConstraints()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    
+    
+    
+    
+    public func configureCell(with recipe: Recipe) {
+        recipeNameLabel.text = recipe.name
+        recipeImageView.image = UIImage(named: recipe.imageName)
+        descriptionLabel.text = recipe.summary
+    }
+    
+    
+    private func configureRecipeImageViewConstraints() {
+        addSubview(recipeImageView)
+        recipeImageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([recipeImageView.topAnchor.constraint(equalTo: self.topAnchor), recipeImageView.leadingAnchor.constraint(equalTo: leadingAnchor), recipeImageView.trailingAnchor.constraint(equalTo: trailingAnchor), recipeImageView.heightAnchor.constraint(equalToConstant: 350)])
+        
+    }
+    
+    
+    private func configureRecipeNameLabel() {
+        addSubview(recipeNameLabel)
+        recipeNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([recipeNameLabel.centerXAnchor.constraint(equalTo: recipeImageView.centerXAnchor), recipeNameLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 10), recipeNameLabel.heightAnchor.constraint(equalToConstant: 30)])
+        
+    }
+    
+    private func configureDescriptionLabelConstraints() {
+        addSubview(descriptionLabel)
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([descriptionLabel.centerXAnchor.constraint(equalTo: centerXAnchor), descriptionLabel.widthAnchor.constraint(lessThanOrEqualToConstant: 300), descriptionLabel.topAnchor.constraint(equalTo: recipeImageView.bottomAnchor, constant: 25)])
+    }
+    
+    
+    
     
 }
